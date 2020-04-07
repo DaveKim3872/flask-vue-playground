ad05b859a888142d9eba0fd24c3f7d1b65fab0b0
金大为 <mrscarf1@outlook.com>
2020-04-07 10:20:14 +0800

    sqlite (user) api

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -8,9 +13,13 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # 注册 blueprint
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
+
+from app import models
